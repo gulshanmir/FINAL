839817569cb43630fb6041d2d16f824adf65a193
gulshanmir <gulshansm.code.edu.az>
2022-12-26 02:51:17 +0400

    changes in index

---
+++ assets/js/about.js
@@ -0,0 +1,54 @@
+let menu2=document.querySelector('#menu2');
+let sidebar=document.querySelector('#sidebar');
+let close=document.querySelector('#close');
+
+menu2.addEventListener('click',()=>{
+    sidebar.classList.add('active')
+    this.style.visibility='hidden'
+})
+
+close.addEventListener('click',()=>{
+    sidebar.classList.remove('active')
+})
+let search=document.querySelector('#search');
+let search2=document.querySelector('#search2');
+
+search.addEventListener('click',()=>{
+    search2.classList.toggle('active')
+})
+
+
+$('.client').slick({
+    dots: false,
+    arrows:true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
