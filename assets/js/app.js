9cb865a18aba6ef6087fa47dff025b6fbac465c5
gulshanmir <gulshansm.code.edu.az>
2022-12-25 13:58:40 +0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,171 @@
+let menu2=document.querySelector('#menu2');
+let sidebar=document.querySelector('#sidebar');
+let close=document.querySelector('#close');
+
+menu2.addEventListener('click',()=>{
+    sidebar.classList.add('active')
+    this.style.visibility='hidden'
+})
+
+close.addEventListener('click',()=>{
+    sidebar.classList.remove('active')
+})
+
+let register=document.querySelector('#register')
+let login2=document.querySelector('#login2');
+ 
+register.addEventListener('click',()=>{
+login2.classList.toggle('active')
+
+})
+
+let search=document.querySelector('#search');
+let search2=document.querySelector('#search2');
+
+search.addEventListener('click',()=>{
+    search2.classList.toggle('active')
+})
+
+$('.slider').slick({
+    dots: false,
+    arrows:true,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:4000,
+    pauseOnHover:false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
+  $('.slider2').slick({
+    dots: true,
+    arrows: false,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
+
+  $('.slider3').slick({
+    dots: false,
+    arrows:true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 2,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
+
+  $('.slider4').slick({
+    dots: false,
+    arrows:true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      
+    ]
+  });
+                  
+  
---
+++ assets/js/basket.js
@@ -0,0 +1,19 @@
+let menu2=document.querySelector('#menu2');
+let sidebar=document.querySelector('#sidebar');
+let close=document.querySelector('#close');
+
+menu2.addEventListener('click',()=>{
+    sidebar.classList.add('active')
+    this.style.visibility='hidden'
+})
+
+close.addEventListener('click',()=>{
+    sidebar.classList.remove('active')
+})
+
+let search=document.querySelector('#search');
+let search2=document.querySelector('#search2');
+
+search.addEventListener('click',()=>{
+    search2.classList.toggle('active')
+})
