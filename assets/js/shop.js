07ae1a0f52fb703f219e6cd2039462217969ff13
gulshanmir <gulshansm.code.edu.az>
2022-12-26 21:29:05 +0400

    shop and basket

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -17,3 +17,24 @@ let search2=document.querySelector('#search2');
 search.addEventListener('click',()=>{
     search2.classList.toggle('active')
 })
+
+
+if (localStorage.getItem('products')===null){
+    localStorage.setItem('products',JSON.stringify([]))
+}
+let buttons=document.querySelectorAll('.icon');
+for(let icon of buttons){
+    icon.onclick=function(e){
+        e.preventDefault();
+        let pr_name=e.target.previousElementSibling.previousElementSibling.innerHTML;
+        let pr_price=e.target.previousElementSibling.innerHTML;
+        let pr_image=e.target.parentElement.previousElementSibling.src;
+        let basket=JSON.parse(localStorage.getItem('products'))
+        basket.push({Name:pr_name,
+        Price:pr_price,
+         Image:pr_image,
+        Count:1 })
+        
+         localStorage.setItem('products',JSON.stringify(basket));
+    }
+}
