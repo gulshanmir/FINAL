b34538a60b3dd51dc63d2c4ce43669a284906af7
gulshanmir <gulshansm.code.edu.az>
2022-12-27 21:51:31 +0400

    shop/js

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,12 +1,26 @@
-if(localStorage.getItem('products')===null) {
-    localStorage.setItem('products',JSON.stringify([]))
-}
+let menu2=document.querySelector('#menu2');
+let sidebar=document.querySelector('#sidebar');
+let close=document.querySelector('#close');
 
-let buttons=document.querySelectorAll('.btn');
-for(let btn of buttons){
-    btn.onclick=function(e){
-        e.preventDefault();
-        let pr_name=e.target.previousElementSibling.previousElementSibling;
-        console.log(pr_name);
-    }
-}
+menu2.addEventListener('click',()=>{
+    sidebar.classList.add('active')
+    this.style.visibility='hidden'
+})
+
+close.addEventListener('click',()=>{
+    sidebar.classList.remove('active')
+})
+
+let register=document.querySelector('#register')
+let login2=document.querySelector('#login2');
+ 
+register.addEventListener('click',()=>{
+  login2.classList.toggle('active')
+})
+
+let search=document.querySelector('#search');
+let search2=document.querySelector('#search2');
+
+search.addEventListener('click',()=>{
+    search2.classList.toggle('active')
+})
