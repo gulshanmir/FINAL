fd94f45fbd4ee8b9b9aba55eef34e245c030b429
gulshanmir <gulshansm.code.edu.az>
2022-12-28 20:13:05 +0400

    table

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -1,5 +1,14 @@
 function GetItems(){
     let basket=JSON.parse(localStorage.getItem('products'));
+
+    if(basket.length===0){
+
+    }
+    else{
+      document.querySelector('.table')
+    }
+
+
     let html='';
     for(let item of basket){
    html+=`
@@ -8,7 +17,9 @@ function GetItems(){
    <th scope="row">${item.Id}</th>
    <td> <img src=${item.Image} alt=""></td>
    <td>${item.Name}</td>
-   <td>${item.count}</td>
+   <td>
+   <input type="number" value=${item.Count}>
+   </td>
    <td>${item.Price}</td>
  </tr>
    
