c5138b23850a28c0dc0986e74c9d287b93c0b4c6
gulshanmir <gulshansm.code.edu.az>
2022-12-28 16:22:12 +0400

    shop

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -29,37 +29,49 @@ let buttons=document.querySelectorAll('.btn');
  for(let btn of buttons){
     btn.onclick=function(e){
         e.preventDefault();
-        let pr_id = e.target.parentElement.parentElement.id;
 
-        let pr_name=e.target.previousElementSibling.previousElementSibling.innerHTML;
-
-        let pr_price = e.target.previousElementSibling.innerHTML;
-        let pr_image = e.target.parentElement.previousElementSibling.src;
+        let pr_id=e.target.parentElement.parentElement.id;
+        let pr_name=e.target.parentElement.firstChild.innerHTML;
+        let pr_price=e.target.previousElementSibling.innerHTML;
+        let pr_image=e.target.parentElement.previousElementSibling.src;
         
 
         let  basket=JSON.parse(localStorage.getItem('products'));
         let exist_prod=basket.find(pr=>pr.Id==pr_id);
+        if(exist_prod===undefined){
+            basket.push({
+                Id: pr_id,
+                Name: pr_name,
+                Price: pr_price,
+                Image: pr_image,
+                Count:1
+            })
+            document.querySelector('#alert p').innerHTML='Product was added to cart successfully!';
+            document.getElementById('alert').style.right='20px';
+        }
+        else{
+            exist_prod.Count +=1;
+        }
 
-        basket.push({
-            Id: pr_id,
-            Name: pr_name,
-            Price: pr_price,
-            Image: pr_image,
-            Count:1
-        })
+       
         localStorage.setItem('products',JSON.stringify(basket));
+        document.getElementById('alert').style.right='20px'
+        BasketCount();
+        setTimeout(()=>{
+            document.getElementById('alert').style.right='-500px'
+
+        },1500)
 
 
 
-
-
-
-
-
-    
-    
-    
     
     
     }
  }
+
+
+ function BasketCount(){
+    let basket=JSON.parse(localStorage.getItem('products'));
+    document.getElementById('count').innerHTML=basket.length;
+ }
+ BasketCount();
