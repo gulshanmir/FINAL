aba391a1801a7a56fb0376dc72928be9e2e99754
gulshanmir <gulshansm.code.edu.az>
2022-12-28 19:03:44 +0400

    a

--- assets/js/shop.js
+++ assets/js/shop.js
@@ -1,24 +1,4 @@
-let menu2=document.querySelector('#menu2');
-let sidebar=document.querySelector('#sidebar');
-let close=document.querySelector('#close');
 
-menu2.addEventListener('click',()=>{
-    sidebar.classList.add('active')
-    this.style.visibility='hidden'
-})
-
-close.addEventListener('click',()=>{
-    sidebar.classList.remove('active')
-})
-
-
-
-let search=document.querySelector('#search');
-let search2=document.querySelector('#search2');
-
-search.addEventListener('click',()=>{
-    search2.classList.toggle('active')
-})
 
 if(localStorage.getItem('products')===null){
     localStorage.setItem('products',JSON.stringify([]))
@@ -31,9 +11,9 @@ let buttons=document.querySelectorAll('.btn');
         e.preventDefault();
 
         let pr_id=e.target.parentElement.parentElement.id;
-        let pr_name=e.target.parentElement.firstChild.innerHTML;
+        let pr_name=e.target.previousElementSibling.previousElementSibling.innerHTML;
         let pr_price=e.target.previousElementSibling.innerHTML;
-        let pr_image=e.target.parentElement.previousElementSibling.src;
+        let pr_image=e.target.parentElement.previousElementSibling.firstChild.src;
         
 
         let  basket=JSON.parse(localStorage.getItem('products'));
