351da07ec07e4383779db58b9ad1fcc43e568de1
gulshanmir <gulshansm.code.edu.az>
2022-12-28 19:31:33 +0400

    table

--- assets/js/basket.js
+++ assets/js/basket.js
@@ -3,6 +3,7 @@ function GetItems(){
     let html='';
     for(let item of basket){
    html+=`
+   
    <tr>
    <th scope="row">${item.Id}</th>
    <td> <img src=${item.Image} alt=""></td>
@@ -10,8 +11,17 @@ function GetItems(){
    <td>${item.count}</td>
    <td>${item.Price}</td>
  </tr>
-
+   
+   
+   
+   
+   
+   
+   
    `
+   
+
+  
     }
 
     document.querySelector('table tbody').innerHTML=html
